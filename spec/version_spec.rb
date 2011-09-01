4f6b31bd8c66bc9a2f3127690a5b398385db96bf
David Czarnecki <dczarnecki@agoragames.com>
2011-09-01 12:47:05 -0400

    Bumping version. Updating CHANELOG.

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,6 +3,6 @@ require 'improved_logging/version'
 
 describe ImprovedLogging do
   it "should be the correct version" do
-    ImprovedLogging::VERSION.should == '1.0.0'
+    ImprovedLogging::VERSION.should == '1.0.1'
   end
 end
